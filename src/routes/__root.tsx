074133fbc6468c73dc60223198ccfa027ac62c68
Daniele Armando Sileoni <danielesileoni4@gmail.com>
2024-07-15 05:03:58 -0300

    test[production]

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,13 +1,22 @@
 import { Footer } from '@/components/common/Footer';
 import { AuthContext } from '@/hooks/useAuth';
 import { Outlet, createRootRouteWithContext } from '@tanstack/react-router';
-import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 import '../index.css';
 import { NotFoundPage } from '@/pages/NotFoundPage';
+import React from 'react';
 
 type RouterContext = {
   authentication: AuthContext;
 };
+
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === 'production'
+    ? () => null
+    : React.lazy(() =>
+        import('@tanstack/router-devtools').then((res) => ({
+          default: res.TanStackRouterDevtools,
+        })),
+      );
 
 export const Route = createRootRouteWithContext<RouterContext>()({
   component: () => (
